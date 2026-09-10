b82317b2ee8ea2aa89313b1d366165483cbb9aa5
chendavinay15-collab <chendavinay.23.cse@anits.edu.in>
2026-09-11 00:19:12 +0530

    Fixed compilation error

--- src/main/java/devops/App.java
+++ src/main/java/devops/App.java
@@ -1,6 +1,10 @@
 package devops;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello DevOps"
+
+        System.out.println("Hello DevOps");
+
     }
 }
